db15c18d1d50fba7d1ead9c98789520e407cef49
RyanFlesher <r_flesher@u.pacific.edu>
2025-10-05 17:00:50 -0700

    THe balls now move across screen and have a cooldown.

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -10,18 +10,47 @@ public class BallLauncher extends GraphicsProgram{
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int SIZE = 25;
 	
+	private static final int SPEED = 2;
+	private static final int MS = 50;
+	private Timer t;
+	private ArrayList<GOval> balls;
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		for (GOval ball : balls) {
+			ball.move(SPEED, 0);
+		}
+	}
+	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		requestFocus();
 	}
 	
 	public void run() {
+		t = new Timer(MS,this);
 		addMouseListeners();
+		t.start();
+		balls = new ArrayList<GOval>();
 	}
 	
+	private boolean canSpawnBall() {
+        // If no balls exist, we can always spawn
+        if (balls.isEmpty()) return true;
+
+        // Otherwise, all must have x >= 100
+        for (GOval b : balls) {
+            if (b.getX() < 100) return false;
+        }
+        return true;
+    }
+	
 	public void mousePressed(MouseEvent e) {
-		GOval ball = makeBall(SIZE/2, e.getY());
-		add(ball);
+		if (canSpawnBall()) {
+			GOval ball = makeBall(SIZE/2, e.getY());
+			add(ball);
+			balls.add(ball);
+		}
 	}
 	
 	public GOval makeBall(double x, double y) {
